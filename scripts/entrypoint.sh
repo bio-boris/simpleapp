107b9e4391ce83d7c85012f7a03fa027d3c5afaa
bio-boris <bio-boris@users.noreply.github.com>
2019-01-16 15:12:36 -0800

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -21,6 +21,16 @@ elif [ "${1}" = "async" ] ; then
   sh ./scripts/run_async.sh
 elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
+  echo "Initialize module"
+  cd /data
+  curl -s http://bioseed.mcs.anl.gov/~chenry/kmer.tgz|tar xzf -
+  ln -s /data/kmer/Release70 /data/kmer/ACTIVE/Release70
+  ln -s /data/kmer/Release70 /data/kmer/DEFAULT
+  if [ -d kmer ] ; then
+  	touch __READY__
+  else
+    echo "Init failed"
+  fi
 elif [ "${1}" = "bash" ] ; then
   bash
 elif [ "${1}" = "report" ] ; then
